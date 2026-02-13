c9319f7fd508e14859500628a146daf54756bb29
novendra27 <adi.novendra.p@gmail.com>
2026-02-13 09:31:55 +0700

    Development: Fix and debug producer logic

--- src/main/java/javadev/project/producer/service/TransactionService.java
+++ src/main/java/javadev/project/producer/service/TransactionService.java
@@ -8,7 +8,9 @@ import javadev.project.producer.dto.transaction.TransactionRequest;
 import javadev.project.producer.entity.product;
 import javadev.project.producer.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,15 +25,23 @@ public class TransactionService {
     @Autowired
     private Validator validator;
 
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${kafka.topic.sales-transaction}")
+    private String topic;
+
     /**
      * Processes a new transaction request
      * Validates the transaction data, checks product availability and stock,
      * then sends the transaction data to Kafka topic for processing by consumer
      * 
      * @param transactionRequest the transaction data including date and items
-     * @return TransactionRequest containing the same transaction data that will be sent to Kafka
+     * @return TransactionRequest containing the same transaction data that will be
+     *         sent to Kafka
      * @throws ConstraintViolationException if validation fails
-     * @throws ResponseStatusException if product is not found or insufficient stock
+     * @throws ResponseStatusException      if product is not found or insufficient
+     *                                      stock
      */
     public TransactionRequest createTransaction(TransactionRequest transactionRequest) {
         // Validate the incoming request
@@ -52,14 +62,13 @@ public class TransactionService {
             if (product.getCurrentStock() < item.getQty()) {
                 throw new ResponseStatusException(
                         HttpStatus.BAD_REQUEST,
-                        "Insufficient stock for product: " + product.getProductName() + 
-                        ". Available: " + product.getCurrentStock() + ", Requested: " + item.getQty());
+                        "Insufficient stock for product: " + product.getProductName() +
+                                ". Available: " + product.getCurrentStock() + ", Requested: " + item.getQty());
             }
         }
 
-        // TODO: Implementasi producer to Kafka
-        // Send transactionRequest to Kafka topic
-        // Example: kafkaTemplate.send("transaction-topic", transactionRequest);
+        // Send transaction data to Kafka topic
+        kafkaTemplate.send(topic, transactionRequest);
 
         return transactionRequest;
     }
